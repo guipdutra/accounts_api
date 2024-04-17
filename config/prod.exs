96aac9d31928d26ed6ed7c946599f7d0a66f07c9
Guilherme Dutra <guipereira07@gmail.com>
2024-04-17 11:02:14 -0300

    remove database name

--- config/prod.exs
+++ config/prod.exs
@@ -22,6 +22,5 @@ config :accounts_api, AccountsApiWeb.Endpoint,
 config :accounts_api, AccountsApi.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: {:system, "DATABASE_URL"},
-  database: "",
   ssl: true,
   pool_size: 2
